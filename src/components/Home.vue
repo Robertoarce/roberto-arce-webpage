a35fd855e82c70bc9f023b8f272faf7691c503b9
Roberto Arce <roberto_arce_@hotmail.com>
2023-10-07 18:23:46 +0200

    test1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,19 +9,22 @@
     <Breadcrumb class="absolute pt-36 pl-4 left-0 right-0 z-12" />
     <div>
 
-      <Cube class="relative inset-0" />
+      <!-- <Cube class="relative inset-0" /> -->
     </div>
   </div>
 </template>
 <script>
 
-import Cube from './cube.vue'
+// import Cube from './cube.vue'
 import Navbar from './Navbar.vue'
 import Breadcrumb from './bread.vue'
 import Drawer from './drawer.vue'
 
 export default {
-  components: { Cube, Navbar, Breadcrumb, Drawer }
+  components: {
+    // Cube,
+    Navbar, Breadcrumb, Drawer
+  }
 }
 </script>
 <style ></style>
