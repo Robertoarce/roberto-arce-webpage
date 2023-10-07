cce055496521307e633e125abf8bf8e9a094c70b
Roberto Arce <roberto_arce_@hotmail.com>
2023-10-07 18:04:32 +0200

    added bred and drwaer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,11 @@
   <div>
 
     <div>
+
       <Navbar class="absolute top-0 left-0 right-0 z-10" />
     </div>
-    <Breadcrumb class="absolute top-16 left-0 right-0 z-12" />
+    <Drawer class="absolute top-40  left-0 right-0 z-113" />
+    <Breadcrumb class="absolute pt-36 pl-4 left-0 right-0 z-12" />
     <div>
 
       <Cube class="relative inset-0" />
@@ -16,12 +18,11 @@
 import Cube from './cube.vue'
 import Navbar from './Navbar.vue'
 import Breadcrumb from './bread.vue'
+import Drawer from './drawer.vue'
 
 export default {
-  components: { Cube, Navbar, Breadcrumb }
+  components: { Cube, Navbar, Breadcrumb, Drawer }
 }
 </script>
 <style ></style>
 
- 
- 
--- src/components/bread.vue
+++ src/components/bread.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="flex p-14" aria-label="Breadcrumb">
+    <nav class="flex  " aria-label="Breadcrumb">
       <ol class="inline-flex items-center space-x-1 md:space-x-3">
         <li class="inline-flex items-center" v-for="(item, index) in breadcrumbItems" :key="index">
           <div class="flex items-center">
