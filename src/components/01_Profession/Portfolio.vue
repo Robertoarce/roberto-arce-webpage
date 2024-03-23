131fbd3c7a6c5adce267be6153cf4b4b0afeda41
Roberto Arce <roberto_arce_@hotmail.com>
2024-03-23 01:53:50 +0100

    adding a better portfolio page

--- src/components/01_Profession/Portfolio.vue
+++ src/components/01_Profession/Portfolio.vue
@@ -1,67 +1,52 @@
 <template> 
   <div class="bg-gray-700 p-10 overflow-auto">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      <div
-        v-for="(project, projectIndex) in projects"
-        :key="projectIndex"
-        class="flex flex-col bg-white rounded-lg shadow-lg   "
-      >
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-8 auto-rows-fr items-stretch ">
+      <div v-for="(project, projectIndex) in projects" :key="projectIndex" 
+      class="flex flex-col bg-white rounded-lg shadow-lg pb-5 px-5   ">
+        
+      <div class="text-center 1/6">
+          <h3 class="font-semibold text-base md:text-xl m-5 line-clamp-2">{{ project.title }}</h3>
+        </div>
 
-       <!-- TTITLE -->
-       <div class=" text-center">
-         <h3 class="font-semibold text-xl m-5">{{ project.title }}</h3>
-       </div>
+        <div class="   justify-center   items-center  pt-5       ">
+          <template v-if="imageModulesArray.length" >
+            <template v-for="({ path, url }, index) in imageModulesArray" :key="index" class="">
+               
 
-         <!-- Images Display Start -->
-          <div class="flex flex-auto justify-center   object-cover   ">
-            <div v-for="(imageName, index) in project.images" :key="index" class="p-2">
-              <img :src="`/src/assets/screenshots/${imageName}`" :alt="`Image for ${project.title}`" 
-              class="  rounded-xl max-w-full max-h-full"  >
+                <img v-if="project.images.includes(path.split('/').pop())"
+                :src="url"
+                :alt="`Loaded image from ${path.split('/').pop()}`"
+                class="rounded         h-max-5/6 ">
+              
+            </template>
+          </template>
+        </div>
+
+
+
+       
+
+          <div class="mt-3 flex flex-row flex-grow description-container  max-h-max " style="max-height: fit-content; overflow-y: auto;">
+            <div class="text-gray-700    text-xs md:text-base m-5  " v-html="project.description"></div>
+          </div>
+
+          <div class="grow h-1/6 pt-2">
+          
+          <div class=" p-3 items-left justify-start space-y-4  ">
+            <a v-if="project.git_link && project.git_link !== ''" :href="project.git_link" target="_blank" class="inline-flex items-center justify-center px-4 py-2 mr-4 border border-transparent text-xs font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700">
+              Git Repository
+            </a>
+            <a v-if="project.notebook_url && project.notebook_url !== ''" :href="project.notebook_url" target="_blank" class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-xs font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-700">
+              View Notebook
+            </a>
+          </div>
+          
+          <div class="items-end">
+            <div class="p-3 flex flex-wrap">
+              <img v-for="tech in project.technologies" :key="tech" :src="generateBadgeUrl(tech)" :alt="tech" class="h-6 mr-2 mt-2">
             </div>
           </div>
-          <!-- Images Display End -->
-
-
-        <!-- TEXT-->
-        <div class="p-6 flex flex-col flex-grow overflow-auto  ">
-         
-          <!-- DESCRIPTION -->
-          <p class="text-gray-700 text-base mb-4" v-html="project.description"></p>
-        </div>
-
-        <!-- Git NBviewer + badges --> 
-
-        <div class="ml-5 pt-2  items-left justify-start space-y-4">
-
-          <!-- GIT -->
-          <a v-if="project.git_link && project.git_link !== ''" :href="project.git_link" target="_blank" 
-            class="inline-flex  items-center justify-center px-4 py-2 mr-4 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700">
-            <svg class="github-icon mr-2 -ml-1 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.86 2.33.66.07-.52.28-.86.51-1.06-1.8-.2-3.69-.9-3.69-4 0-.88.31-1.6.82-2.17-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.57.82 1.29.82 2.17 0 3.1-1.89 3.8-3.69 4 .29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
-            </svg>  
-            Git Repository
-          </a>
-
-          <!-- Notebook -->
-          <a v-if="project.notebook_url && project.notebook_url !== ''" :href="project.notebook_url" target="_blank" 
-            class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white
-             bg-yellow-500 hover:bg-yellow-700">
-            View Notebook
-          </a>
-        </div>
-
-        <!-- Badges container -->
-        <div class="items-end">
-
-          <div class="p-3 flex flex-wrap">
-            <img
-            v-for="tech in project.technologies"
-            :key="tech"
-            :src="generateBadgeUrl(tech)"
-            :alt="tech"
-            class="h-6 mr-2 mt-2 "
-            />
-          </div>
+          
         </div>
       </div>
     </div>
@@ -69,9 +54,32 @@
 </template>
 
 
+
+<style>
+/* Scrollbar styles */
+.description-container ::-webkit-scrollbar {
+  width: 2px;
+}
+
+.description-container ::-webkit-scrollbar-track {
+  background: transparent; /* Optional: just in case you want a transparent track */
+}
+
+.description-container ::-webkit-scrollbar-thumb {
+  background: #ff6347; /* A nice tomato color for the thumb */
+  border-radius: 2px;
+}
+
+/* For other browsers that support scrollbar styling */
+.description-container {
+  scrollbar-width: thin;
+  scrollbar-color: #ff6347 transparent; /* thumb and track color */
+}
+</style>
+
 <script>
-import fetchRepositoriesData from '@utils/repositories.js'; 
-const imageModules = import.meta.globEager('@/src/assets/screenshots/*.jpg'); // Adjust the path as necessary
+import fetchRepositoriesData from '@utils/repositories.js';  
+const imageModules = import.meta.globEager('/src/assets/screenshots/*.*');
 // const imageModules = import.meta.globEager('/src/assets/screenshots/*.jpg');
 
 export default {
@@ -83,14 +91,16 @@ export default {
   created() {
     fetchRepositoriesData().then((projectsData) => {
       this.projects = projectsData;
+      console.log('Image Modules:', imageModules); 
     }).catch((error) => {
       console.error('Error fetching projects data:', error);
     });
   },
   methods: {
     getImage(imageName) { 
+    // const imagePath = `./assets/screenshots/${imageName}`;
     const imagePath = `./src/assets/screenshots/${imageName}`;
-      return imageModules[imagePath]?.default;
+      return imageModules[imagePath]?.default || '';
     },
     generateBadgeUrl(tech) {
       const base = "https://img.shields.io/badge/";
@@ -101,7 +111,16 @@ export default {
     
       return `${base}${label}-${color}?style=${style}&logo=${logo}`;
     }
+  },
+  
+  computed: {
+  imageModulesArray() {
+    return Object.entries(imageModules).map(([path, module]) => {
+      return { path, url: module.default }; // Adjust this line if your module structure is different
+    });
   }
+},
+
 }
 </script>
  
--- src/utils/repositories.js
+++ src/utils/repositories.js
@@ -2,6 +2,7 @@ const repositoriesData = [
   {
     title: 'Fully Dynamic Table Creation process with Dbt', 
     description: "\
+    <b>TLDR:</b>  It reads database structure in compilation time, and adjusts the SQL querie to the structure, avoiding missing datasets/tables/values errors. <br><br> \
       This approach addresses errors arising from missing tables, misnaming sources,\
       or neglecting to add new tables, all while dynamically aggregating tables during the repository building process.<br><br>\
       This DBT script showcases an advanced use case of dynamically building SQL queries based on the availability of certain datasets and columns.\
@@ -16,21 +17,40 @@ const repositoriesData = [
     in_startpage: true,
     notebook_url:'',
   },
-  // {
-  //   title: 'Benchmarking Bayesian Frameworks', 
-  //   description: "\
-  //     Evaluating the performance of various Bayesian frameworks including Stan, PyMC3, TensorFlow Probability, and Julia \
-  //     in terms of execution time and memory consumption. <br> This assessment utilizes synthetic data applied to Bayesian regression models.\
-  //     The models are estimated using Hamiltonian Monte Carlo (HMC) methods, specifically leveraging the No-U-Turn Sampler (NUTS) for efficiency improvements.\
-  //     <br> The primary objective is to identify the most time-efficient framework under these specific conditions.\
-  //     This is followed by an evaluation of aspects critical for production, including potential deployment issues, as well as the availability and quality of community and documentation support.",
-  //   technologies: ['amazon aws', 'Pymc3','stan', 'TensorFlow',  'Python', 'Pandas', 'numpy', 'jupyter', 'Git'],
-  //   ongoing: false,
-  //   images:['img_1.jpg'],  
-  //   in_startpage:false,
-  //   notebook_url:'dd',
-  // },
-  
+  {
+    title: 'Fast-Learning Challenge: Learn Docker in 5 days.', 
+    description: "\
+    <b>TLDR:</b>  I had to learn and apply Docker for a technical test in less than 5 days. <br><br> \
+      Your mission is to provide insights on sales.\
+      The challenge consists in creating a docker image so that the users will be able to quick use.<br>\
+      <br><h1> <b>Guidelines: </b></h3>\
+      There are four datasets :<br>\
+      <ul> \
+        <b>* Products:</b> a list of available products.<br>\
+        <b>* Items:</b> a list of items.<br>\
+        <b>* Orders:</b> a list of customer orders on the website.<br>\
+        <b>* Customers:</b> a list of customers.<br>\
+        </ul><br>\
+        <h2><b>TO DO:</b></h2>\
+        <ul> \
+        <li> - Create and insert data in PostgreSQL.</li> \
+        <li> - Each day we want to compute summary statistics by customers (spending, orders etc.)</li> \
+        <li> - Create a script to compute for a given day these summary statistics.</li>\
+        <li> - Query directly the dbt</li> \
+        <li> - Have premaid queries</li> \
+        <li> - Run that script over the necessary period to inject historic data. </li>\
+        <li> - Then, identify the top customers</li>\
+        <li> - How many customers are repeaters ?</li>\
+        <li> - Package your script in Docker container so that it can be run each day. </li>\
+        </ul>\
+        <br>A docker-compose.yml and a CLI is expected to get stats for a specific day.\
+            ",
+    technologies: ['docker', 'postgresql', 'pandas'],
+    images:['docker.png'],  
+    git_link: 'https://github.com/Robertoarce/free2move-Test-technique',
+    ongoing: false,
+    in_startpage: true, 
+  }, 
 
 ];
 
