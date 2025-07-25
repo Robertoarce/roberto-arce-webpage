81e33ca01cb157c4c42f3ef51613e11746e1173f
Roberto Arce <roberto_arce_@hotmail.com>
2025-07-25 20:16:01 +0200

    change of dns name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/showcase-webpage/",
+  base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/assets/diplomas'),
